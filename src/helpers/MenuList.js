0c7c03c5bcbb313f2c49455140bd61f947f90820
DarynaSm <92335128+DarynaSm@users.noreply.github.com>
2023-06-08 10:54:14 +0300

    Add files via upload

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Minicheesecakes from "../assets/cheesecakes.jpg";
+import fruittarts from "../assets/fruittarts.jpg";
+import matchatarts from "../assets/MatchaTarts.jpg";
+import tiramisu from "../assets/tiramisu.jpg";
+import Pavlova from "../assets/pavlova.jpg";
+import praline from "../assets/praline.jpg";
+
+export const MenuList = [
+  {
+    name: "Mini cheese cakes",
+    image: Minicheesecakes,
+    price: 15.99,
+  },
+  {
+    name: "Fruit tarts",
+    image: fruittarts,
+    price: 11.99,
+  },
+  {
+    name: "Matcha tarts",
+    image: matchatarts,
+    price: 16.53,
+  },
+  {
+    name: "Tiramisu",
+    image: tiramisu,
+    price: 17.99,
+  },
+  {
+    name: "Pavlova",
+    image: Pavlova,
+    price: 4.99,
+  },
+  {
+    name: "praline",
+    image: praline,
+    price: 19.99,
+  },
+];
